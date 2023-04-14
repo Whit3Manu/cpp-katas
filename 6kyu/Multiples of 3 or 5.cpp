89e1409517ba135aa3b6e199943709d5b930ece2
Whit3Manu <129676300+Whit3Manu@users.noreply.github.com>
2023-04-14 17:19:52 +0200

    Create Multiples of 3 or 5.cpp

---
+++ 6kyu/Multiples of 3 or 5.cpp
@@ -0,0 +1,14 @@
+int solution(int num) {
+  if (num < 0)
+    return 0;
+  
+  int sum = 0;
+  
+  for (int i = 0; i < num; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      sum += i;
+    }
+  }
+  
+  return sum;
+}
